9c9c071ef85ffda2e0cc9f658dc15436d011c5f6
KIKO <veronikadmitrieva1402.gmail.com>
2026-06-20 08:35:50 +0300

    Background

--- MoodBoardGenerator.Client/obj/Debug/net9.0/MoodBoardGenerator.Client.AssemblyInfo.cs
+++ MoodBoardGenerator.Client/obj/Debug/net9.0/MoodBoardGenerator.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoodBoardGenerator.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c31113ceafdbe1966eda272c606341e80b4a95e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddd2e45ce096be98d753adea828d7f471487088d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoodBoardGenerator.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoodBoardGenerator.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
